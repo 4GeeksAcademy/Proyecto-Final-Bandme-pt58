0f32da3e38e9131357bd181e188a8d626479323c
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-12-16 03:16:03 +0000

    creacion del signup

---
+++ src/front/pages/SignUp.jsx
@@ -0,0 +1,81 @@
+import { Link } from "react-router-dom";
+
+export const SignUp = () => {
+	return (
+		<>
+			<nav className="bg-dark text-light text-center py-2 mt-auto">
+				<div className="container-fluid">
+					<h1 className="navbar-brand text-center w-100 h1">BandMe</h1>
+				</div>
+			</nav>
+<div>
+	<p className="text-center mt-3"><b>What kind of account would you like?</b></p>
+</div>
+
+			<div className="container mt-4">
+				<form className="row g-3">
+					<div className="col-md-12">
+						<label htmlFor="inputEmail" className="form-label">
+							Email:
+						</label>
+						<input
+							type="email"
+							className="form-control"
+							id="inputEmail"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<label htmlFor="inputPassword" className="form-label">
+							Password:
+						</label>
+						<input
+							type="password"
+							className="form-control"
+							id="inputPassword"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<label htmlFor="firstName" className="form-label">
+							First name:
+						</label>
+						<input
+							type="text"
+							className="form-control"
+							id="firstName"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<label htmlFor="lastName" className="form-label">
+							Last name:
+						</label>
+						<input
+							type="text"
+							className="form-control"
+							id="lastName"
+						/>
+					</div>
+
+					<div className="col-md-12">
+						<label htmlFor="inputZip" className="form-label">
+							ZIP code:
+						</label>
+						<input
+							type="text"
+							className="form-control"
+							id="inputZip"
+						/>
+					</div>
+
+					<div className="col-12">
+						<button type="submit" className="btn btn-secondary w-100">
+							Sign up
+						</button>
+					</div>
+				</form>
+			</div>
+		</>
+	);
+};
