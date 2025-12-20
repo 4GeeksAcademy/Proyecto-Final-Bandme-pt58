08d2930fb42732d6c73b6d5fa5435a653ae933e0
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-12-20 00:33:09 +0000

    cambios

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
 							<span class="navbar-toggler-icon"></span>
 						</button>
 						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center align-items-center">
 								<li class="nav-item">
 									<a class="nav-link active" aria-current="page" href="#">HOME</a>
 								</li>
@@ -26,7 +26,7 @@ export const Navbar = () => {
 									<a class="nav-link" href="#">LIBRERY</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link" href="#"><img src="https://i.pinimg.com/736x/b1/40/cf/b140cf1ff6dfc4002d6ac79b12a40d92.jpg" className="mt-3 rounded-circle" alt="..." width="40px" height="40px" /></a>
+									<a class="nav-link" href="#"><img src="https://i.pinimg.com/1200x/ac/ef/7a/acef7a10207e7d98a43b994e463afa27.jpg" className="mt-3 rounded-circle" alt="..." width="40px" height="40px" /></a>
 								</li>
 
 							</ul>
