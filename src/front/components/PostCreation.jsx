f80b2809a135b5cabf82b0b196b3d23e2345810b
Charliekofg <charliekofg@hotmail.com>
2026-01-11 21:57:18 +0000

    updating branch

---
+++ src/front/components/PostCreation.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function PostCreation ({ user_id }) {
+    const [text, setText] = useState("");
+    const [image, setImage] = useState(null);
+    const [publishHome, setPublishHome] = useState(false);
+
+
+}
