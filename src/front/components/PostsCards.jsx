c23c6a657fcd36ae95f4cc0456be34cd99a57b91
Charliekofg <charliekofg@hotmail.com>
2026-01-13 01:28:46 +0000

    updated the model, the PostsCards and the Home, now you can mailto

--- src/front/components/PostsCards.jsx
+++ src/front/components/PostsCards.jsx
@@ -1,11 +1,28 @@
-export default function PostsCard ( {image, title, text} ){
-    return(
-        <div className="col-md-4 mb-4">
-            <div className="card border border-secondary shadow" style={{ width: "18rem" }}>
-                <img src={image} className="card-img-top" alt={title}/>
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{text}</p>
+export default function PostsCard({ image, text, userEmail, userName }) {
+    return (
+        <div className="col-md-4 mb-4 d-flex justify-content-center">
+            <div className="card border border-secondary shadow h-100" style={{ width: "18rem" }}>
+                {image && (
+                    <img
+                        src={image}
+                        className="card-img-top"
+                        alt="Post"
+                        style={{ height: "200px", objectFit: "cover" }}
+                    />
+                )}
+
+                <div className="card-body d-flex flex-column">
+                    <h5 className="card-title text-primary">@{userName}</h5>
+                    <p className="card-text flex-grow-1">{text}</p>
+
+
+                    <a
+                        href={`mailto:${userEmail}?subject=Reach out from Bandme&body=Hi ${userName}, I would like to get in touch with you in regard to your Post.`}
+                        className="btn btn-outline-primary mt-auto"
+                    >
+                        <i className="fa-regular fa-envelope me-2"></i>
+                        Contact the author
+                    </a>
                 </div>
             </div>
         </div>
